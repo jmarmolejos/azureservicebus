653e4032dd0996baa817a6eb1f49dba653cae851
Jose Marmolejos <Jose Marmolejos>
2017-04-09 21:14:29 +0200

    Add files.

---
+++ queue.reader/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using queue.model;
+
+namespace queue.reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workerConfigurationManager = new WorkerConfigurationManager();
+            var workerConfiguration = workerConfigurationManager.GetWorkerConfiguration();
+
+            var connectionString = workerConfiguration.ConnectionString;
+            var queueName = workerConfiguration.QueueName;
+            var deadLetterQueueName = QueueClient.FormatDeadLetterPath(queueName);
+            
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine($"Message body: {message.GetBody<String>()}");
+                Console.WriteLine($"Message id: {message.MessageId}");
+            });
+
+            //RetrieveLoop(client);
+
+            //PeekMessages(client, stopwatch);
+
+            Console.ReadLine();
+        }
+
+        private static void RetrieveLoop(QueueClient client)
+        {
+            while (true)
+            {
+                var message = client.Receive();
+                Console.WriteLine($"Message body: {message.GetBody<String>()}");
+                Console.WriteLine($"Message id: {message.MessageId}");
+
+                message.Complete();
+            }
+        }
+
+        private static void PeekMessages(QueueClient client)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var messages = client.PeekBatch(250).ToList();
+
+            messages.ForEach(message =>
+            {
+                Console.WriteLine($"Message body: {message.GetBody<String>()}");
+                Console.WriteLine($"Message id: {message.MessageId}");
+            });
+
+            Console.WriteLine($"Read {messages.Count} messages.");
+            Console.WriteLine($"Finished, took {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ queue.writer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using queue.model;
+
+namespace queue.writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workerConfigurationManager = new WorkerConfigurationManager();
+            var workerConfiguration = workerConfigurationManager.GetWorkerConfiguration();
+
+            var connectionString = workerConfiguration.ConnectionString;
+            var queueName = workerConfiguration.QueueName;
+            
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            var orderList = new List<BrokeredMessage>();
+
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 250; i++)
+            {
+                var order = new Order
+                {
+                    CreationDate = DateTime.UtcNow,
+                    OrderNumber = Guid.NewGuid().ToString(),
+                    CustomerId = Guid.NewGuid().ToString(),
+                    SalesStatus = SalesStatus.Sold
+                };
+
+                var jsonContent = JsonConvert.SerializeObject(order);
+                
+                var message = new BrokeredMessage(jsonContent);
+                orderList.Add(message);
+            }
+
+            client.SendBatch(orderList);
+            Console.WriteLine($"Finished, took {stopwatch.ElapsedMilliseconds} ms");
+
+        }
+    }
+}
